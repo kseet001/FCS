3e730be116f1aa5509d319a0e66d6781d42b0ff1
Kenny Seet <kseet001@gmail.com>
2018-11-06 12:07:50 +0800

    Encapsulated the logic in a func and tested with additional test data

--- Homework8/homework_qn2.py
+++ Homework8/homework_qn2.py
@@ -2,29 +2,43 @@ from Crypto.Cipher import AES
 from binascii import hexlify
 
 ''' Define the plaintext, IV and key '''
-m1 = "\xff" * int(16)  # 16 bytes
-key = "\x00" * 16  # 128 bits key
-IV = "\x00" * 16  # 128 bits IV
+#m1 = b"\xa0" * int(16)  # 16 bytes
+key = b"\x00" * 16  # 128 bits key
+IV = b"\x00" * 16  # 128 bits IV
 
-''' To calculate CBC-MAC of message m, encrypt m in CBC mode with zero IV and obtain last 16bytes'''
-encryptor = AES.new(key.encode(), AES.MODE_CBC, IV.encode())
 
-''' Obtain t1 '''
-t1 = encryptor.encrypt(m1.encode())
+def collision(message):
+    m1 = message
 
-''' Find c, where (t1 xor c) = m1 '''
-a = bytearray(t1)
-b = bytearray(m1.encode())
-c = bytearray(len(a))
-for i in range(len(a)):
-    c[i] = a[i] ^ b[i]
-m2 = m1.encode()+c     # This step still need to work on it
+    ''' To calculate CBC-MAC of message m, encrypt m in CBC mode with zero IV and obtain last 16bytes'''
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
 
-''' Test1 '''
-encryptor = AES.new(key.encode(), AES.MODE_CBC, IV.encode()) # reinitiate encryptor
-t2 = encryptor.encrypt(m2)[16:]
+    ''' Obtain t1 '''
+    t1 = encryptor.encrypt(m1)
 
-print("\nPlaintext 1: %s" %(hexlify(m1.encode())))
-print("CBC-MAC, t1: %s" %(hexlify(t1)))
-print("Plaintext 2: %s" %(hexlify(m2)))
-print("CBC-MAC, t2: %s" %(hexlify(t2)))
+    ''' Find c, where (t1 xor c) = m1 '''
+    a = bytearray(t1)
+    b = bytearray(m1)
+    c = bytearray(len(a))
+    for i in range(len(a)):
+        c[i] = a[i] ^ b[i]
+    m2 = m1 + c
+
+    ''' Obtain t2 '''
+    encryptor = AES.new(key, AES.MODE_CBC, IV)  # reinitiate encryptor
+    t2 = encryptor.encrypt(m2)[16:]
+
+    print("\nSame CBC-MAC is found for m1 and m2!")
+    print("m1: \t\t\t%s" % (hexlify(m1)))
+    print("CBC-MAC(m1): \t%s" % (hexlify(t1)))
+    print("m2: \t\t\t%s" % (hexlify(m2)))
+    print("CBC-MAC(m2): \t%s" % (hexlify(t2)))
+
+def main():
+    print("\nQuestion 2:")
+    collision(b"\x00" * int(16))
+    collision(b"\xa0" * int(16))
+    collision(b"\xff" * int(8) + B"\x00" * int(8))
+
+if __name__ == '__main__':
+    main()

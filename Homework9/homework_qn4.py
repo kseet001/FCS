512f4990daf700f9752771f5d400c145f542eec4
erwinerwinn <finderwinhere@gmail.com>
2018-11-14 09:09:25 +0800

    HW question 4 - used histogram.

--- Homework9/homework_qn4.py
+++ Homework9/homework_qn4.py
@@ -1,41 +1,45 @@
 import random
-import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
+import plotly.plotly as py
+import plotly.tools as tls
 
-s = [None]
+s = []
 
 def naiveRandom127():
-    generatedNumber = random.randint(0, 254)
+    generatedNumber = random.getrandbits(8)
     naiveRandom = generatedNumber % 128
     return naiveRandom
 
-def plotGraph(s):
-    t = np.arange(0, 512, 1)
-    fig, ax = plt.subplots()
-    ax.plot(s, t)
-
-    ax.set(xlabel='Random', ylabel='attempt',
-           title='Naive Random Distribution')
-    ax.grid()
-
-    fig.savefig("test.png")
+def plotHistogram(s):
+    plt.hist(s, bins=512)
+    plt.title("Naive Random Generator")
+    plt.ylabel('Frequency')
+    plt.xlabel('Numbers')
+    plt.figure(figsize=(2, 3))
     plt.show()
 
-# def plotHistogram(s):
-#     plt.hist(s, bins=int(512))
-#     fig=matplotlib.pyplot.gcf()
-#     fig.savefig("test2.png")
-
 def main():
-    for i in range(1, 512):
+    for i in range(0, 512):
         s.append(naiveRandom127())
 
-    print(str(s))
-
-    plotGraph(s)
-    #plotHistogram(s)
+    print(s)
+    #plotGraph(s)
+    plotHistogram(s)
 
 if __name__ == "__main__":
     main()
 
+
+# def plotGraph(s):
+#     t = np.arange(0, 512, 1)
+#     fig, ax = plt.subplots()
+#     ax.plot(s, t)
+#
+#     ax.set(xlabel='Random', ylabel='attempt',
+#            title='Naive Random Distribution')
+#     ax.grid()
+#
+#     fig.savefig("test.png")
+#     plt.show()

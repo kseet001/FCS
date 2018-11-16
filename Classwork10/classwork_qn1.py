bb4c2e050aa3284da3be223aa33141ee812d352d
erwinerwinn <finderwinhere@gmail.com>
2018-11-16 21:36:06 +0800

    implemented algo

--- Classwork10/classwork_qn1.py
+++ Classwork10/classwork_qn1.py
@@ -16,15 +16,10 @@ b_secret = 15
 
 # Transmission and Computation
 a_send = (sharedBase ** a_secret) % sharedPrime
-b_compute = a_send ** b_secret
-bobSends = (sharedBase ** b_secret) % sharedPrime
-a_compute = bobSends ** a_secret
-
+b_sends = (sharedBase ** b_secret) % sharedPrime
 
 print("A sends\t\t", a_send)
-print("B computes\t", b_compute)
-print("B sends\t\t", bobSends)
-print ("A computes\t", a_compute)
+print("B sends\t\t", b_sends)
 
 print()
-print ("Shared Secret ", (sharedBase ** (a_secret * b_secret)) % sharedPrime)
+print("Shared Secret ", (sharedBase ** a_secret) ** b_secret % sharedPrime)

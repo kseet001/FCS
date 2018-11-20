d8ddd242d71c499a4e46afa5254a513f76b53612
erwinerwinn <finderwinhere@gmail.com>
2018-11-20 13:58:21 +0800

    1234

--- Homework10/homework_qn4.py
+++ Homework10/homework_qn4.py
@@ -1,27 +1,34 @@
 import random
+from hashlib import sha256
 
 
-
-def gcd(a, b):
+def coprime(a, b):
     while b != 0:
         a, b = b, a % b
     return a
 
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    g, y, x = egcd(b%a,a)
-    return (g, x - (b//a) * y, y)
 
+def extended_gcd(aa, bb):
+    lastremainder, remainder = abs(aa), abs(bb)
+    x, lastx, y, lasty = 0, 1, 1, 0
+    while remainder:
+        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+        x, lastx = lastx - quotient * x, x
+        y, lasty = lasty - quotient * y, y
+    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+
+
+# Euclid's extended algorithm for finding the multiplicative inverse of two numbers
 def modinv(a, m):
-    g, x, y = egcd(a, m)
+    g, x, y = extended_gcd(a, m)
     if g != 1:
-        raise Exception('No modular inverse')
-    return x%m
+        raise Exception('Modular inverse does not exist')
+    return x % m
+
 
 def is_prime(num):
     if num == 2:
-         return True
+        return True
     if num < 2 or num % 2 == 0:
         return False
     for n in range(3, int(num ** 0.5) + 2, 2):
@@ -29,69 +36,113 @@ def is_prime(num):
             return False
     return True
 
+
 def generate_keypair(p, q):
     if not (is_prime(p) and is_prime(q)):
         raise ValueError('Both numbers must be prime.')
     elif p == q:
         raise ValueError('p and q cannot be equal')
-    # n = pq
+
     n = p * q
 
     # Phi is the totient of n
     phi = (p - 1) * (q - 1)
-    print("Phi: ", phi)
 
     # Choose an integer e such that e and phi(n) are coprime
-    e = random.randrange(1, phi)
+    e = 3
 
     # Use Euclid's Algorithm to verify that e and phi(n) are comprime
-    g = gcd(e, phi)
+    g = coprime(e, phi)
+
     while g != 1:
         e = random.randrange(1, phi)
-        g = gcd(e, phi)
+        g = coprime(e, phi)
 
     # Use Extended Euclid's Algorithm to generate the private key
-    d = modinv(e,phi)
+    d = modinv(e, phi)
 
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
-def encrypt(pk, plaintext):
+
+def encrypt(privatek, plaintext):
     # Unpack the key into it's components
-    key, n = pk
+    key, n = privatek
+
     # Convert each letter in the plaintext to numbers based on the character using a^b mod m
-    cipher = pow(int(plaintext),key) % n#[(ord(char) ** key) % n for char in plaintext]
+
+    numberRepr = [ord(char) for char in plaintext]
+    print("Number representation before encryption: ", numberRepr)
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+
     # Return the array of bytes
     return cipher
 
-def sign(d,m):
-    key, n = d
-    return pow(m,key)% n
 
-def verify(e,m,c):
-    if ((pow(c,e)%n) == m ):
-        print("YES")
-        return 1
+def decrypt(publick, ciphertext):
+    # Unpack the key into its components
+    key, n = publick
+
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    numberRepr = [pow(char, key, n) for char in ciphertext]
+    plain = [chr(pow(char, key, n)) for char in ciphertext]
+
+    print("Decrypted number representation is: ", numberRepr)
+
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+def hashFunction(message):
+    hashed = sha256(message.encode("UTF-8")).hexdigest()
+    print("Hash of the message: ", hashed)
+    return hashed
+
+
+def verify(receivedHashed, message):
+    ourHashed = hashFunction(message)
+    if receivedHashed == ourHashed:
+        print("Verification successful: ", )
+        print(receivedHashed, " = ", ourHashed)
     else:
-        print("NO")
-        return 0
 
-if __name__ == '__main__':
+        print("Verification failed")
+        print(receivedHashed, " != ", ourHashed)
+
+
+def main():
+    # p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    # q = int(input("Enter another prime number (Not one you entered above): "))
     p = 71
     q = 89
 
-    m1 = 12312412312323
-    m2 = 2312414241239735563242
-
+    print("Generating your public/private keypairs now . . .")
     public, private = generate_keypair(p, q)
+
     print("Your public key is ", public, " and your private key is ", private)
-    m3 = (m1 * m2) % public[1]
-    print("m3: ",m3)
-    encryptedMsg = encrypt(public,m3)
-    print("Encrypted message = ", encryptedMsg)
-    print("Signed Message = ", sign(private,encryptedMsg))
+    message = input("Enter a message to encrypt with your private key: ")
+    print("")
+
+    hashed = hashFunction(message)
 
 
+    print("Encrypting message with private key ", private, " . . .")
+    encrypted_msg = encrypt(private, hashed)
+    print("Your encrypted hashed message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    # print(encrypted_msg)
+
+    print("")
+    print("Decrypting message with public key ", public, " . . .")
+
+    decrypted_msg = decrypt(public, encrypted_msg)
+    print("Your decrypted message is:")
+    print(decrypted_msg)
+
+    print("")
+    print("Verification process . . .")
+    verify(decrypted_msg, message)
 
 
+main()

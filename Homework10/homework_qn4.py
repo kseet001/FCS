0c80dda3326e984649cbc45c26a610323cea8278
erwinerwinn <finderwinhere@gmail.com>
2018-11-20 14:55:27 +0800

    1234

--- Homework10/homework_qn4.py
+++ Homework10/homework_qn4.py
@@ -1,41 +1,40 @@
+'''
+Let p = 71, q = 89, n = pq, e = 3. First find the corresponding private RSA key d.
+Then compute the signature on m1 = 5416, m2 = 2397, and m3 = m1m2 (mod n)
+using the basic RSA operation. Show that the third signature is equivalent to the
+product of the first two signatures.
+'''
+
 import random
-from hashlib import sha256
 
-
-def coprime(a, b):
+def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
 
 
-def extended_gcd(aa, bb):
-    lastremainder, remainder = abs(aa), abs(bb)
-    x, lastx, y, lasty = 0, 1, 1, 0
-    while remainder:
-        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
-        x, lastx = lastx - quotient * x, x
-        y, lasty = lasty - quotient * y, y
-    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    g, y, x = egcd(b % a, a)
+    return (g, x - (b // a) * y, y)
 
 
-# Euclid's extended algorithm for finding the multiplicative inverse of two numbers
 def modinv(a, m):
-    g, x, y = extended_gcd(a, m)
+    g, x, y = egcd(a, m)
     if g != 1:
-        raise Exception('Modular inverse does not exist')
+        raise Exception('No modular inverse')
     return x % m
-
 
 def is_prime(num):
     if num == 2:
-        return True
+         return True
     if num < 2 or num % 2 == 0:
         return False
     for n in range(3, int(num ** 0.5) + 2, 2):
         if num % n == 0:
             return False
     return True
-
 
 def generate_keypair(p, q):
     if not (is_prime(p) and is_prime(q)):
@@ -52,97 +51,55 @@ def generate_keypair(p, q):
     e = 3
 
     # Use Euclid's Algorithm to verify that e and phi(n) are comprime
-    g = coprime(e, phi)
-
+    g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
-        g = coprime(e, phi)
+        g = gcd(e, phi)
 
     # Use Extended Euclid's Algorithm to generate the private key
-    d = modinv(e, phi)
+    d = modinv(e,phi)
 
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
-
-def encrypt(privatek, plaintext):
-    # Unpack the key into it's components
-    key, n = privatek
-
-    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
-
-    numberRepr = [ord(char) for char in plaintext]
-    print("Number representation before encryption: ", numberRepr)
-    cipher = [pow(ord(char), key, n) for char in plaintext]
-
-    # Return the array of bytes
-    return cipher
+def sign(d, m):
+    return (m ** d[0]) % n
 
 
-def decrypt(publick, ciphertext):
-    # Unpack the key into its components
-    key, n = publick
+def verify(e, m, c):
+    tmp1 = pow(c,e) % n
+    tmp2 = m
 
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
-    numberRepr = [pow(char, key, n) for char in ciphertext]
-    plain = [chr(pow(char, key, n)) for char in ciphertext]
-
-    print("Decrypted number representation is: ", numberRepr)
-
-    # Return the array of bytes as a string
-    return ''.join(plain)
-
-
-def hashFunction(message):
-    hashed = sha256(message.encode("UTF-8")).hexdigest()
-    print("Hash of the message: ", hashed)
-    return hashed
-
-
-def verify(receivedHashed, message):
-    ourHashed = hashFunction(message)
-    if receivedHashed == ourHashed:
-        print("Verification successful: ", )
-        print(receivedHashed, " = ", ourHashed)
+    if tmp1 == tmp2:
+        return True
     else:
+        return False
 
-        print("Verification failed")
-        print(receivedHashed, " != ", ourHashed)
+if __name__ == '__main__':
 
-
-def main():
-    # p = int(input("Enter a prime number (17, 19, 23, etc): "))
-    # q = int(input("Enter another prime number (Not one you entered above): "))
+    '''
+    Question 4
+    '''
     p = 71
     q = 89
+    n = p * q
+    e = 3
 
-    print("Generating your public/private keypairs now . . .")
-    public, private = generate_keypair(p, q)
+    print("value of n: ", n)
 
-    print("Your public key is ", public, " and your private key is ", private)
-    message = input("Enter a message to encrypt with your private key: ")
-    print("")
+    m1 = 5416
+    m2 = 2397
+    m3 = (m1 * m2) % n
 
-    hashed = hashFunction(message)
+    public, private = generate_keypair(p,q)
 
+    m1_signed = sign(private, m1)
+    m2_signed = sign(private, m2)
+    m3_signed = sign(private, m3)
 
-    print("Encrypting message with private key ", private, " . . .")
-    encrypted_msg = encrypt(private, hashed)
-    print("Your encrypted hashed message is: ")
-    print(''.join(map(lambda x: str(x), encrypted_msg)))
-    # print(encrypted_msg)
+    print("m1_signed: ", m1_signed)
+    print("m2_signed: ", m2_signed)
+    print("m3_signed: ", m3_signed)
 
-    print("")
-    print("Decrypting message with public key ", public, " . . .")
-
-    decrypted_msg = decrypt(public, encrypted_msg)
-    print("Your decrypted message is:")
-    print(decrypted_msg)
-
-    print("")
-    print("Verification process . . .")
-    verify(decrypted_msg, message)
-
-
-main()
+    print()
+    print("m1_signed * m2_signed: ", (m1_signed*m2_signed) % n)
+    print("m3_signed: ", m3_signed % n)

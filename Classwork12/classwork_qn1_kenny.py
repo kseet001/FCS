59cace7cae0fd0ead88a9af3d5493df4118372ed
kseet001 <kseet001@gmail.com>
2018-12-01 01:33:37 +0800

    finalised PKI implementation

--- Classwork12/classwork_qn1_kenny.py
+++ Classwork12/classwork_qn1_kenny.py
@@ -1,13 +1,11 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES
 from Crypto.Hash import SHA
 from datetime import datetime
 from datetime import timedelta
-#from Crypto.Cipher import PKCS1_v1_5
-from Crypto.Signature import PKCS1_v1_5
-import base64
+from Crypto.Cipher import PKCS1_v1_5
+#from Crypto.Signature import PKCS1_v1_5
+from Crypto import Random
 import json
-import random
 
 class Certificate(object):
 
@@ -27,15 +25,22 @@ class Certificate(object):
         self.not_before = datetime.now()
         self.subject = subject
         self.issuer = issuer
-        self.public_key = public_key.publickey().exportKey('PEM')
+        #self.public_key = public_key.publickey().exportKey('PEM')
+        self.public_key = public_key
 
         cert_string = self.subject + self.issuer + str(self.not_before) + str(self.not_after) + str(self.public_key) + str(self.signature)
         json_str = json.dumps(cert_string)
 
-        h = SHA.new(json_str.encode()).digest()
+        h = SHA.new(json_str.encode())
+        dsize = h.digest_size
+        self.sentinel = Random.new().read(15+dsize)
         signer = PKCS1_v1_5.new(issuer_private_key)
-        self.signature = signer.sign(h)
-        #self.signature = signer.encrypt(h)
+        #self.signature = signer.sign(h)
+        self.signature = signer.encrypt(h.digest())
+
+
+    def get_cert_without_sig(self):
+        return self.subject + self.issuer + str(self.not_before) + str(self.not_after) + str(self.public_key)
 
     def get_cert(self):
         return self.subject + self.issuer + str(self.not_before) + str(self.not_after) + str(self.public_key) + str(self.signature)
@@ -46,15 +51,17 @@ def main():
     root_key, server_key = RSA.generate(2048), RSA.generate(2048)
     root_CA = Certificate()
     root_CA.create_certificate("root", "root", root_key, root_key)
-    print(root_CA.get_cert())
+    #print(root_CA.get_cert())
 
-    print("")
+    #print("")
     server = Certificate()
     server.create_certificate("server", "root", server_key, root_key)
-    print(server.get_cert())
+    #print(server.get_cert())
 
-    print("\nAlice request from server")
+    print("\nAlice communicates with server")
     print("Server sends Alice the server's certificate")
+
+    print("\n\n=== Checking server cert ===")
 
     print("\nAlice obtained: ")
     print(server.get_cert())
@@ -65,14 +72,44 @@ def main():
     print("\nAlice get public key of the root CA:")
     print(root_CA.public_key)
 
-    json_str = json.dumps(root_CA.get_cert())
+    json_str = json.dumps(server.get_cert_without_sig())
     h = SHA.new(json_str.encode()).digest()
 
     checker = PKCS1_v1_5.new(root_CA.public_key)
-    if checker.verify(h, root_CA.signature):
-        print("OK")
+    tmp = checker.decrypt(server.signature, server.sentinel)
+    print("\n Alice verifies the signature: ")
+    print("Decrypted signature: ", tmp)
+    print("Hash of certificate without signature: ", h)
+    if tmp == h:
+        print("OK - Server verified")
     else:
-        print("NOT OKAY")
+        print("NOT OK")
+
+    ''' Checking root cert '''
+
+    print("\n\n=== Checking root cert ===")
+    print("\nAlice obtained: ")
+    print(root_CA.get_cert())
+
+    print("\nAlice wants to verify the signature:")
+    print(root_CA.signature)
+
+    print("\nAlice get public key of the root CA:")
+    print(root_CA.public_key)
+
+    json_str = json.dumps(root_CA.get_cert_without_sig())
+    h = SHA.new(json_str.encode()).digest()
+
+    checker = PKCS1_v1_5.new(root_CA.public_key)
+    tmp = checker.decrypt(root_CA.signature, root_CA.sentinel)
+    print("\n Alice verifies the signature: ")
+    print("Decrypted signature: ", tmp)
+    print("Hash of certificate without signature: ", h)
+    if tmp == h:
+        print("OK - CA verified")
+    else:
+        print("NOT OK")
+
 
 if __name__ == '__main__':
     main()

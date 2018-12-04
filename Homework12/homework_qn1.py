2044d2c359074e6051979ec8aa34e8d369ae448f
erwinerwinn <finderwinhere@gmail.com>
2018-12-04 10:11:47 +0800

    classwork12 - RAW - need to be refactored

---
+++ Homework12/homework_qn1.py
@@ -0,0 +1,298 @@
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA, SHA256, HMAC
+from datetime import datetime
+from datetime import timedelta
+from Crypto.Cipher import PKCS1_v1_5 as PKCS1
+from Crypto.Signature import PKCS1_v1_5 as PKCS2
+import json
+import random
+import math
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from Crypto import Random
+from binascii import hexlify
+
+# Choose a random, 16-byte IV.
+iv = Random.new().read(AES.block_size)
+
+# Convert the IV to a Python integer.
+iv_int = int(hexlify(iv), 16)
+
+class Peer(object):
+    def __init__(self, key, name):
+
+        self.name = name
+
+        if name == "Alice":
+            self.key_send_enc = SHA256.new((key + "Enc Alice to Bob").encode()).digest()
+            self.key_recv_enc = SHA256.new((key + "Enc Bob to Alice").encode()).digest()
+            self.key_send_auth = SHA256.new((key + "Auth Alice to Bob").encode()).digest()
+            self.key_recv_auth = SHA256.new((key + "Auth Bob to Alice").encode()).digest()
+        elif name == "Bob":
+            self.key_send_enc = SHA256.new((key + "Enc Bob to Alice").encode()).digest()
+            self.key_recv_enc = SHA256.new((key + "Enc Alice to Bob").encode()).digest()
+            self.key_send_auth = SHA256.new((key + "Auth Bob to Alice").encode()).digest()
+            self.key_recv_auth = SHA256.new((key + "Auth Alice to Bob").encode()).digest()
+        elif name == "Eve":
+            self.key_send_enc = SHA256.new((key + "Enc Bob to Alice").encode()).digest()
+            self.key_recv_enc = SHA256.new((key + "Enc Alice to Bob").encode()).digest()
+            self.key_send_auth = SHA256.new((key + "Enc Bob to Alice").encode()).digest()
+            self.key_recv_auth = SHA256.new((key + "Enc Alice to Bob").encode()).digest()
+
+        ctr = Counter.new(AES.block_size * 8, initial_value=iv_int)
+        self.encryptor = AES.new(self.key_send_enc, AES.MODE_CTR, counter=ctr)
+        self.decryptor = AES.new(self.key_recv_enc, AES.MODE_CTR, counter=ctr)
+
+
+    def send(self, msg):
+        mac = HMAC.new(self.key_send_auth, msg.encode(), SHA256).hexdigest() # calculate the mac of the msg
+
+        protected_msg = self.encryptor.encrypt(msg + mac)
+
+        #print("msg: ", msg)
+        #print("mac: ", mac)
+        print("%s sends: %s" %(self.name, protected_msg))
+
+        return protected_msg # type of protected_msg is ’str’
+
+    def receive(self, protected_msg):
+
+        decrypted_msg = self.decryptor.decrypt(protected_msg)
+        msg = decrypted_msg[:len(decrypted_msg)-64]
+        mac1 = decrypted_msg[len(decrypted_msg)-64:]
+
+        # check msg integrity
+        mac2 = HMAC.new(self.key_recv_auth, msg, SHA256).hexdigest().encode()
+
+        if mac1 == mac2:
+            print(
+                "%s decrypts and got the following message: %s" % (self.name, msg))  # successfully recovered plaintext
+        else:
+            print("%s decrypts and MAC did not match - Message has been tampered!" %(self.name))
+
+
+
+
+
+def is_prime(n):
+    while True:
+        isprime = True
+        for x in range(2, int(math.sqrt(n) + 1)):
+            if n % x == 0:
+                isprime = False
+                break
+
+        if isprime:
+            break
+
+        n += 1
+    return n
+
+class Certificate(object):
+
+    def __init__(self):
+        self.subject = ""
+        self.issuer = ""
+        self.not_before = ""
+        self.not_after = ""
+        self.public_key = ""
+        self.signature = ""
+        self.sentinel = ""
+
+
+    def create_certificate(self, subject, issuer, public_key, issuer_private_key):
+
+        self.not_after = datetime.now() + timedelta(days=3)
+        self.not_before = datetime.now()
+        self.subject = subject
+        self.issuer = issuer
+        #self.public_key = public_key.publickey().exportKey('PEM')
+        self.public_key = public_key
+
+        cert_string = self.subject + self.issuer + str(self.not_before) + str(self.not_after) + str(self.public_key) + str(self.signature)
+        json_str = json.dumps(cert_string)
+
+        h = SHA.new(json_str.encode())
+        dsize = h.digest_size
+        self.sentinel = Random.new().read(15+dsize)
+        signer = PKCS1.new(issuer_private_key)
+        #self.signature = signer.sign(h)
+        self.signature = signer.encrypt(h.digest())
+
+
+    def get_cert_without_sig(self):
+        return self.subject + self.issuer + str(self.not_before) + str(self.not_after) + str(self.public_key)
+
+    def get_cert(self):
+        return self.subject + self.issuer + str(self.not_before) + str(self.not_after) + str(self.public_key) + str(self.signature)
+
+
+def main():
+
+    root_key, server_key = RSA.generate(2048), RSA.generate(2048)
+    root_CA = Certificate()
+    root_CA.create_certificate("root", "root", root_key, root_key)
+    #print(root_CA.get_cert())
+
+    #print("")
+    server = Certificate()
+    server.create_certificate("server", "root", server_key, root_key)
+    #print(server.get_cert())
+
+    print("\nAlice communicates with server")
+    print("Server sends Alice the server's certificate")
+
+    print("\n\n=== Checking server cert ===")
+
+    print("\nAlice obtained: ")
+    print(server.get_cert())
+
+    print("\nAlice wants to verify the signature:")
+    print(server.signature)
+
+    print("\nAlice get public key of the root CA:")
+    print(root_CA.public_key)
+
+    json_str = json.dumps(server.get_cert_without_sig())
+    h = SHA.new(json_str.encode()).digest()
+
+    checker = PKCS1.new(root_CA.public_key)
+    tmp = checker.decrypt(server.signature, server.sentinel)
+    print("\n Alice verifies the signature: ")
+    print("Decrypted signature: ", tmp)
+    print("Hash of certificate without signature: ", h)
+    if tmp == h:
+        print("OK - Server verified")
+    else:
+        print("NOT OK")
+
+    ''' Checking root cert '''
+
+    print("\n\n=== Checking root cert ===")
+    print("\nAlice obtained: ")
+    print(root_CA.get_cert())
+
+    print("\nAlice wants to verify the signature:")
+    print(root_CA.signature)
+
+    print("\nAlice get public key of the root CA:")
+    print(root_CA.public_key)
+
+    json_str = json.dumps(root_CA.get_cert_without_sig())
+    h = SHA.new(json_str.encode()).digest()
+
+    checker = PKCS1.new(root_CA.public_key)
+    tmp = checker.decrypt(root_CA.signature, root_CA.sentinel)
+    print("\n Alice verifies the signature: ")
+    print("Decrypted signature: ", tmp)
+    print("Hash of certificate without signature: ", h)
+    if tmp == h:
+        print("OK - CA verified")
+    else:
+        print("NOT OK")
+    print("========================\t\t End of Certificate issuance \t\t========================")
+    print("========================\t\t Initiating Key Negotiation Protocol \t\t========================")
+
+    key_A = RSA.generate(2048)
+    key_B = RSA.generate(2048)
+
+    # Alice
+    Sa = 3
+    N = random.randint(0, (2 ** 256) - 1)
+    print("Alice sends Sa=", Sa, " N= ", N)
+    print()
+
+    # Bob
+    Sb = 3
+    s = max(Sa, Sb)
+    assert s <= 2 * Sb
+    # Bob Chooses (g,p,q)
+    q = 5
+    p = 2 * q + 1
+    alpha = random.randint(2, p - 2)
+    g = alpha ** 2 % p
+    # assert
+    assert g != 1
+    assert g != (p - 1)
+    b = random.randint(1, q - 1)
+    B = g ** b % p
+
+    s_str = str(N)
+
+    # Bob send
+    h = SHA.new(s_str.encode())
+    signer = PKCS2.new(key_B)
+    signature = signer.sign(h)
+    print("Bob sends: (g,p,q) = (", g, p, q, ") B=", B, " AUTHbob=", signature)
+    print()
+
+    # Alice receive
+    h = SHA.new(s_str.encode())
+    verifier = PKCS2.new(key_B)
+    # Alice checks AUTHbob
+    if verifier.verify(h, signature):
+        print("Bob's signature is valid")
+    else:
+        print("Bob's signature is not valid")
+
+    # Assert
+    assert (Sa - 1) <= math.log(p, 2)
+    assert math.log(p, 2) <= 2 * Sa
+    assert 2 <= math.log(q, 2) <= 3
+    assert p == is_prime(p)
+    assert q == is_prime(q)
+    assert q | (p - 1) ^ g != 1 ^ (g ** q == 1)
+    assert B != 1 ^ (B ** q != 1)
+    a = random.randint(1, q - 1)
+    A = g ** a % p
+
+    h = SHA.new(s_str.encode())
+    signer = PKCS2.new(key_A)
+    signature = signer.sign(h)
+    Kp = (B ** a) % p
+    alice_K = SHA256.new(str(Kp).encode()).hexdigest()
+    print("Alice sends: A=", A, " AUTHalice=", signature)
+    print()
+
+    # Send to Bob
+    h = SHA.new(s_str.encode())
+    verifier = PKCS2.new(key_A)
+
+    # Bob checks AUTHalice
+    if verifier.verify(h, signature):
+        print("Alice's signature is valid")
+    else:
+        print("Alice's signature is not alid")
+    assert A != 1
+    assert A ** q % p == 1
+
+    Kp2 = (A ** b) % p
+    bob_K = SHA256.new(str(Kp2).encode()).hexdigest()
+
+    print()
+    print("Alice's key computation= %s\nBob's key computation= = %s" % (alice_K, bob_K))
+    print("========================\t\t End of Key Negotiation \t\t========================")
+    print("========================\t\t Secure Channel Communication using shared key \t\t========================")
+    # Example
+    alice = Peer(alice_K, "Alice")
+    bob = Peer(bob_K, "Bob")
+    eve = Peer("unknown key12345", "Eve")
+
+    print("\nMessage 1:")
+    msg1 = alice.send("Msg from alice to bob")
+    bob.receive(msg1)
+    eve.receive(msg1)
+
+    print("\nMessage 2:")
+    msg2 = alice.send("Another msg from alice to bob")
+    bob.receive(msg2)
+    eve.receive(msg2)
+
+    print("\nMessage 3:")
+    msg3 = bob.send("Hello alice")
+    alice.receive(msg3)
+    eve.receive(msg3)
+
+
+if __name__ == '__main__':
+    main()
